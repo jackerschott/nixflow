780ed0222d80d6140d823bcf30d9c6d4b04519dc
Jona Ackerschott <jona.a99@gmail.com>
2025-05-02 12:26:45 +0200

    nixflow: add step derivations as flake outputs

--- default.nix
+++ default.nix
@@ -10,9 +10,18 @@
         pythonScript = arguments: script: pkgs.writers.writePython3 "run" arguments script;
     };
 
-    lib = {
-        makeStepsPrinter = workflowStepsPath: (workflow: pkgs.writers.writeBashBin "steps" ''
-            printf '%s' '${builtins.toJSON workflow}'
-        '')(import workflowStepsPath { nixflow = self.preamble; inherit pkgs lib; });
+    lib = let
+        collectParentSteps = inputs: (builtins.attrValues (lib.mapAttrs (name: input: input.parentStep) inputs));
+        collectStepRunners = runners: step: if step ? inputs
+            then runners ++ [step.run] ++ (collectParentSteps step.inputs)
+            else runners ++ [step.run];
+    in {
+        makeStepsPrinter = workflowStepsPath:
+            (workflow: pkgs.writers.writeBashBin "steps" '' printf '%s' '${builtins.toJSON workflow}' '')
+            (import workflowStepsPath { nixflow = self.preamble; inherit pkgs lib; });
+
+        makeStepRunners = workflowStepsPath:
+            (workflow: collectStepRunners [] workflow.parentStep)
+            (import workflowStepsPath { nixflow = self.preamble; inherit pkgs lib; });
     };
 }
